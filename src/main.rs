5f044a85d6f7b26237cd1867f50f1c238a240c0e
Thomas Kluyver <takowl@gmail.com>
2015-05-10 14:29:01 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::io::Write;
+use std::collections::LinkedList;
+use std::str::FromStr;
+
+fn input_line(prompt: &str) -> Result<String, std::io::Error> {
+    let mut stdout = std::io::stdout();
+    let mut line = String::new();
+    try!(stdout.write(prompt.as_bytes()));
+    try!(stdout.flush());
+    try!(std::io::stdin().read_line(&mut line));
+    return Ok(line);
+}
+
+fn add(stack: &mut LinkedList<i32>) {
+    let n1 = stack.pop_front().unwrap();
+    let n2 = stack.pop_front().unwrap();
+    stack.push_front(n1 + n2);
+}
+
+fn subtract(stack: &mut LinkedList<i32>) {
+    let n1 = stack.pop_front().unwrap();
+    let n2 = stack.pop_front().unwrap();
+    stack.push_front(n2 - n1);
+}
+
+fn multiply(stack: &mut LinkedList<i32>) {
+    let n1 = stack.pop_front().unwrap();
+    let n2 = stack.pop_front().unwrap();
+    stack.push_front(n1 * n2);
+}
+
+fn divide(stack: &mut LinkedList<i32>) {
+    let n1 = stack.pop_front().unwrap();
+    let n2 = stack.pop_front().unwrap();
+    stack.push_front(n2 / n1);
+}
+
+fn main() {
+    let line = input_line("> ").ok().expect("Failed to read stdin");
+    let mut stack = LinkedList::new();
+    for token in line.trim().split(' ') {
+        match token {
+            "+" => add(&mut stack),
+            "-" => subtract(&mut stack),
+            "*" => multiply(&mut stack),
+            "/" => divide(&mut stack),
+            _ => stack.push_front(i32::from_str(token).ok().unwrap())
+        }
+    }
+    println!("= {}", stack.pop_front().unwrap());
+}
